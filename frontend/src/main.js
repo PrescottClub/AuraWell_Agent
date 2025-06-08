35737da5493e71ffb66ba9a07c069458038f959f
SayHiToYoung <weiyuyang250@outlook.com>
2025-06-08 11:10:41 +0800

    构建后台管理框架，引入echarts并使用mock展示数据成功。

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
+import VChart from 'vue-echarts'
 
 const app = createApp(App)
+app.component('v-chart', VChart)
 app.use(router)
 app.use(Antd)
 app.mount('#app')
